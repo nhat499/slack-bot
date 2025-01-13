745016beb80bf5cbcdf8eb4a828b3acb92efd16d
Nhat Trang <nhat499@uw.edu>
2025-01-13 14:53:42 -0800

    test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,25 @@ import { Elysia, t } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(5000);
 
+/*
+bot setting a message
+POST https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX
+Content-type: application/json
+{
+    "text": "Gotta get the bread and milk!"
+}
+
+*/
+
+app.post("hc", ({ params, headers, body }) => {
+  console.log({
+    params,
+    headers,
+    body,
+  });
+  return "ok";
+});
+
 app.post(
   "/slack/:challenge",
   ({ params, set, body }) => {
