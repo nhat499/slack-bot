2f1f22143421f58c0806b3b46cec11fe5307f907
Nhat Trang <nhat499@uw.edu>
2025-01-13 13:58:10 -0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+app.post(
+  "/slack/:challenge",
+  (req) => {
+    console.log(req.params.challenge);
+
+    return { challenge: req.params.challenge };
+  },
+  {
+    params: t.Object({
+      challenge: t.String(),
+    }),
+  }
+);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
